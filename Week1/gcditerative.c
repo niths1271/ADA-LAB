82f6d6daeb6a466b51e26bea3c097954c1f39c0f
NITHIN.C <71306445+niths1271@users.noreply.github.com>
2021-04-20 15:34:08 +0530

    Update gcditerative.c

--- Week1/gcditerative.c
+++ Week1/gcditerative.c
@@ -10,8 +10,8 @@ int gcd(int a,int b){
     int i=0;
     int m=min(a,b);
     for(i=m;i>=1;i--){
-        if(a%m==0 && b%m==0){
-        return m;
+        if(a%i==0 && b%i==0){
+        return i;
         }
     }
 }
